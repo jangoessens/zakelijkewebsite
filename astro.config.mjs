0d6b7ce8ec7f097cc55a5df65c5cff6a09b64b7e
Jan <jangoessens@hotmail.com>
2025-05-14 23:49:59 +0200

    remove unused package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
-  adapter: staticAdapter(),
   site: 'https://jangoessens.github.io',
   base: '/zakelijkewebsite/', // BELANGRIJK voor GitHub Pages
 });
