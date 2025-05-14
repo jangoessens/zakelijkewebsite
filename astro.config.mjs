68b671fa2f1dca9191b9b63ef20a12e1e95f8bb9
Jan <jangoessens@hotmail.com>
2025-05-15 00:00:29 +0200

    add cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jangoessens.github.io',
-  base: '/zakelijkewebsite/', // BELANGRIJK voor GitHub Pages
+  site: 'https://zakelijke-website.nl',
 });
