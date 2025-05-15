7a4569c78d9f4c9f9fc8a93e66d7dea1489a908b
Jan <jangoessens@hotmail.com>
2025-05-16 00:00:44 +0200

    add dual styling + mockup site 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
 export default defineConfig({
   site: 'https://zakelijke-website.nl',
+  integrations: [vue()],
 });
---
+++ src/components/TypingText.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="typing-text">
+    <span v-if="spelled.length">{{ spelled }}</span>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const props = defineProps({ words: Array })
+
+const spelled = ref('')
+onMounted(() => {
+  const typeWord = async (word) => {
+    for (let i = 0; i < word.length; i++) {
+      spelled.value += word[i]
+      await new Promise((resolve) =>
+        setTimeout(resolve, Math.random() * (400 - 50) + 50)
+      )
+    }
+  }
+
+  const backspaceWord = async () => {
+    while (spelled.value.length > 0) {
+      spelled.value = spelled.value.slice(0, -1)
+      await new Promise((resolve) => setTimeout(resolve, 75))
+    }
+  }
+
+  const loopWords = async () => {
+    while (true) {
+      for (const word of props.words) {
+        await typeWord(word)
+        await new Promise((resolve) => setTimeout(resolve, 1500))
+        await backspaceWord()
+        await new Promise((resolve) => setTimeout(resolve, 500))
+      }
+    }
+  }
+
+  loopWords()
+})
+</script>
