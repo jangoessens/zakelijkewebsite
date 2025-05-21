bb9517bf70755a866f9fa1c9991cac101a16beb9
Jan <jangoessens@hotmail.com>
2025-05-21 17:32:56 +0200

    move all to homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://zakelijke-website.nl',
-  integrations: [vue()],
+  integrations: [vue(), icon()],
 });
---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,31 @@
+<script setup>
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+const carouselConfig = {
+  itemsToShow: 1,
+  wrapAround: true,
+  autoplay: 3000,
+}
+
+const props = defineProps({
+  images: {
+    type: Array,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <Carousel v-bind="carouselConfig">
+    <Slide v-for="imageSlide, index in images" :key="`image-slide-${index}`">
+      <div class="carousel__item"><img :src="imageSlide" /></div>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
+</template>
